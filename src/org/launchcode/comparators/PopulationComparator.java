4a4def1dd82b60e5a0424d47612b1384cd0463d4
Kevin Shutt <kevinshutt@yahoo.com>
2017-12-11 19:44:20 -0600

    Finished studio

--- src/org/launchcode/comparators/PopulationComparator.java
+++ src/org/launchcode/comparators/PopulationComparator.java
@@ -4,14 +4,12 @@ import org.launchcode.City;
 
 import java.util.Comparator;
 
-/**
- * Created by LaunchCode
- */
-public class NameComparator implements Comparator<City> {
+public class PopulationComparator implements Comparator<City> {
 
     @Override
     public int compare(City o1, City o2) {
-
-        return o1.getName().compareTo(o2.getName());
+        return (o2.getPopulation() - (o1.getPopulation()));
     }
+
+
 }
--- src/org/launchcode/comparators/StateComparator.java
+++ src/org/launchcode/comparators/StateComparator.java
@@ -4,14 +4,11 @@ import org.launchcode.City;
 
 import java.util.Comparator;
 
-/**
- * Created by LaunchCode
- */
-public class NameComparator implements Comparator<City> {
+public class StateComparator implements Comparator<City> {
 
     @Override
     public int compare(City o1, City o2) {
-
-        return o1.getName().compareTo(o2.getName());
+        return (o1.getState().compareTo(o2.getState()));
     }
+
 }
